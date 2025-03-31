c24bae2c4e9aeec5d1dc84bcbba422e72247ffb0
Mouhcine Fallahi <mohcinefallahi23@gmail.com>
2025-03-31 10:16:55 +0000

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mfallahi23.github.io/med-project-1/",
+  site: "https://mfallahi23.github.io",
   base: "/med-project-1",
 });
